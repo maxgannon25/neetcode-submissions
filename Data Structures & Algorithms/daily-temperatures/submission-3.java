984170d09ed6c459ede65e654648e3bc8ddbabd0
maxgannon25 <maxgannon25@outlook.com>
2026-08-14 01:40:26 +0100

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i=0; i< temperatures.length; i++){
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+
+                int previous = stack.pop();
+                result[previous] = i - previous;
+
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
