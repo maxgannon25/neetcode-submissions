424ebf3eeba0306201c5828dabf939e57e158b43
maxgannon25 <maxgannon25@outlook.com>
2026-08-27 21:18:47 +0100

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,37 @@
+class Solution {
+     public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()){
+            return false;
+        }
+        
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for (char c: s1.toCharArray()){
+            count1[c - 'a']++;
+        }
+
+        for (int i=0; i< s1.length(); i++){
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(count1, count2)){
+            return true;
+        }
+
+        int l = 0;
+        for (int r=s1.length(); r<s2.length(); r++){
+            count2[s2.charAt(r) - 'a']++;
+            count2[s2.charAt(l) - 'a']--;
+
+            l++;
+
+            if (Arrays.equals(count1, count2)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
