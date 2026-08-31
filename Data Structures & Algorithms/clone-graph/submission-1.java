7ef35d4f02357767e7c5f53f5180f778f5a706de
maxgannon25 <maxgannon25@outlook.com>
2026-08-31 15:39:21 +0100

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.java
@@ -0,0 +1,49 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node dfs(Node node, Map<Node, Node> clones){
+
+        if (node == null){
+            return null;
+        }
+
+        if (clones.containsKey(node)){
+            return clones.get(node);
+        }
+
+        Node clone = new Node(node.val);
+
+        clones.put(node, clone);
+
+        for (Node n: node.neighbors){
+            Node clonedNeighbor = dfs(n, clones);
+            clone.neighbors.add(clonedNeighbor);
+        }
+
+        return clone;
+    }
+
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> clones = new HashMap<>();
+        // YOUR LOGIC HERE
+        return dfs(node, clones);
+    }
+}
