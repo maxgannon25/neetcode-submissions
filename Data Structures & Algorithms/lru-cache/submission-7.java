c52ebb6cdb0e16840d7d180889984a5f295758b7
maxgannon25 <maxgannon25@outlook.com>
2026-09-01 14:45:58 +0100

    Add: lru-cache - submission-7

---
+++ Data Structures & Algorithms/lru-cache/submission-7.java
@@ -0,0 +1,39 @@
+class LRUCache {
+
+    int capacity;
+    List<int[]> cache;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new ArrayList<>();
+    }
+    
+    public int get(int key) {
+
+        for (int i=0; i<cache.size(); i++){
+            if (cache.get(i)[0] == key){
+                int[] tmp = cache.remove(i);
+                cache.add(tmp);
+                return tmp[1];
+            } 
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        for (int i=0; i<cache.size(); i++){
+            if (cache.get(i)[0] == key){
+                int[] tmp = cache.remove(i);
+                tmp[1] = value;
+                cache.add(tmp);
+                return;
+            } 
+        }
+
+        if (this.capacity == cache.size()){
+            cache.remove(0);
+        }
+
+        cache.add(new int[]{key, value});
+    }
+}
