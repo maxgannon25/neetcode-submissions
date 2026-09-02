b33ec999ad5b3d207c5ad466b6add091bbf3b606
maxgannon25 <maxgannon25@outlook.com>
2026-09-02 15:43:51 +0100

    Add: best-time-to-buy-and-sell-stock-ii - submission-3

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int l = 0;
+
+        int maxProfit = 0;
+
+        for (int r=1; r<prices.length; r++){
+
+            int prof = prices[r] - prices[l];
+
+            if (prof > 0){
+                maxProfit += prof;
+            }
+            l = r;
+        }
+
+        return maxProfit;
+    }
+}
